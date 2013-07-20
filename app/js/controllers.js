4736850c8ab86c2b34ce639482f1acc2b57bee7a
mendhak <smaltaf@msn.com>
2013-07-20 18:43:16 +0100

    Demo 04 - Factory call to Apiary and

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,17 +1,27 @@
 'use strict';
 
+var myApp = angular.module('myApp.controllers', []);
+
+/* Factory */
+myApp.factory('fleetTypesFactory', function($http) {
+    //Returning a promise
+    return $http.get('http://flight.apiary.io/v1/Fleet/EventTypes?apiToken=00000000-0000-0000-0000-000000000001');
+});
+
+
 /* Controllers */
+myApp.controller('MyCtrl1', ['$scope', 'fleetTypesFactory', function ($scope, fleetTypesFactory) {
+    
+        //This could take a long time...
+        fleetTypesFactory.success(function (data) {
+            $scope.FleetEventTypes = data.EventTypes;
+        });
 
-angular.module('myApp.controllers', []).
-    controller('MyCtrl1', ['$scope', function($scope) {
-
-        //A property
-        $scope.status = { buildFailed: false };
-
-        //A method
-        $scope.RobJustPushedSomethingToGit = function() {
-            $scope.status = { buildFailed: true };
-        };
-
+        $scope.isEventTypesEmpty = true;
+        
+        //Watching an array for changes
+        $scope.$watch('FleetEventTypes', function() {
+            $scope.isEventTypesEmpty = ($scope.FleetEventTypes == null);
+        }, false);
     }]);
  
