1aa9dd77c7136685c9ef23de848cf167b04723f9
mendhak <smaltaf@msn.com>
2013-07-20 22:29:01 +0100

    Parameter in URL to partial view

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,6 +6,6 @@ angular.module('myApp.controllers', []).
   controller('MyCtrl1', [function() {
 
   }])
-  .controller('MyCtrl2', [function() {
-
+  .controller('MyCtrl2', ['$scope', '$routeParams', function($scope, $routeParams) {
+      $scope.OrgID = $routeParams.organisationId;
   }]);
