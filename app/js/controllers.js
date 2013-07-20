5eecfa895d71154a8b37aeb7ae008aed5d44a172
mendhak <smaltaf@msn.com>
2013-07-20 17:43:42 +0100

    Demo 03 - More on controllers

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,7 +5,13 @@
 angular.module('myApp.controllers', []).
     controller('MyCtrl1', ['$scope', function($scope) {
 
-        $scope.message = "From a controller";
+        //A property
+        $scope.status = { buildFailed: false };
+
+        //A method
+        $scope.RobJustPushedSomethingToGit = function() {
+            $scope.status = { buildFailed: true };
+        };
 
     }]);
  
