a84c2111d85e02b98e970c5b244d40af1d77ffff
mendhak <smaltaf@msn.com>
2013-07-20 23:46:45 +0100

    Demo 07 - simple direcctive

--- app/js/directives.js
+++ app/js/directives.js
@@ -3,9 +3,28 @@
 /* Directives */
 
 
-angular.module('myApp.directives', []).
+var app = angular.module('myApp.directives', [])
+
+    app.
   directive('appVersion', ['version', function(version) {
     return function(scope, elm, attrs) {
       elm.text(version);
     };
   }]);
+
+
+app.directive('hoverBgColor', function() {
+    return {
+        restrict: 'A',
+      
+        link: function (scope, element, attributes) {
+            
+            element.bind('mouseenter', function() {
+                element.css('background-color', attributes.hoverBgColor);
+            });
+            element.bind('mouseleave', function() {
+                element.css('background', 'none');
+            })
+        }
+    };
+})
