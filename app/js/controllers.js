e6ab40512c92ff1f10fd9a775839bef83ba0a510
mendhak <smaltaf@msn.com>
2013-07-21 17:42:00 +0100

    Demo 12 - Unit testing, promises

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,7 +5,15 @@ var myApp = angular.module('myApp.controllers', []);
 /* Factory */
 myApp.factory('fleetTypesFactory', function($http) {
     //Returning a promise
-    return $http.get('http://flight.apiary.io/v1/Fleet/EventTypes?apiToken=00000000-0000-0000-0000-000000000001');
+    return {
+        getStuff: function() {
+            return $http.get('http://flight.apiary.io/v1/Fleet/EventTypes?apiToken=00000000-0000-0000-0000-000000000001')
+                .then(function(result) {
+                    return result.data;
+                });
+
+        }
+    };
 });
 
 
@@ -13,9 +21,9 @@ myApp.factory('fleetTypesFactory', function($http) {
 myApp.controller('MyCtrl1', ['$scope', 'fleetTypesFactory', function ($scope, fleetTypesFactory) {
     
         //This could take a long time...
-        fleetTypesFactory.success(function (data) {
-            $scope.FleetEventTypes = data.EventTypes;
-        });
+    fleetTypesFactory.getStuff().then(function (data) {
+        $scope.FleetEventTypes = data.EventTypes;
+    });
 
         $scope.isEventTypesEmpty = true;
         
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -2,15 +2,56 @@
 
 /* jasmine specs for controllers go here */
 
-describe('controllers', function(){
-  beforeEach(module('myApp.controllers'));
+describe('controllers', function () {
+    beforeEach(module('myApp.controllers'));
+    var ctrl, scope, fakeFleet;
+
+    beforeEach(inject(function ($rootScope, $controller) {
+        scope = $rootScope.$new();
+
+        //Fake factory, no real 'success' method
+        fakeFleet = {
+            getStuff: function () { return { then: jasmine.createSpy() } }
+        };
+
+        spyOn(fakeFleet, 'getStuff').andCallThrough();
+
+        ctrl = $controller('MyCtrl1', { $scope: scope, fleetTypesFactory: fakeFleet });
+    }));
 
 
-  it('should ....', inject(function() {
-    //spec body
-  }));
+    it('Event Types Empty should default to false', inject(function () {
+        expect(scope.isEventTypesEmpty).toBe(true);
+    }));
 
-  it('should ....', inject(function() {
-    //spec body
-  }));
+    it('On init, api should be called', function() {
+        expect(fakeFleet.getStuff).toHaveBeenCalled();
+    });
+
+   
+});
+
+
+describe('Controller gets bad response', function () {
+    
+    beforeEach(module('myApp.controllers'));
+    var ctrl, scope, fakeFleet;
+
+    beforeEach(inject(function ($rootScope, $controller) {
+        scope = $rootScope.$new();
+
+        //Fake factory, no real 'success' method
+        fakeFleet = {
+            getStuff: function () { return { then: jasmine.createSpy() } }
+        };
+
+        ctrl = $controller('MyCtrl1', { $scope: scope, fleetTypesFactory: fakeFleet });
+
+    }));
+
+    it('EventTypesEmpty changes to false when an object is returned', function () {
+        scope.FleetEventTypes = {'EventTypes':['1','2','3']};
+        scope.$apply();
+        expect(scope.isEventTypesEmpty).toBe(false);
+    });
 });
